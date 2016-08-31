3915a39df7c578c310200ea7ffae7eccf9bf3916
chenqiang <393562632@qq.com>
2016-08-31 18:01:23 +0800

    add file

--- chapter_five_biz/src/main/java/springboot/WebApplication.java
+++ chapter_five_biz/src/main/java/springboot/WebApplication.java
@@ -1,12 +1,11 @@
 package springboot;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.context.web.SpringBootServletInitializer;
 
-@EnableAutoConfiguration
+@SpringBootApplication
 public class WebApplication extends SpringBootServletInitializer {
     @Override
     protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
