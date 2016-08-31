3f35294212fd51fba134ee4c717d7ad50ff72c77
chenqiang <393562632@qq.com>
2016-08-31 17:46:00 +0800

    add montior

--- chapter_five_biz/src/main/java/springboot/WebApplication.java
+++ chapter_five_biz/src/main/java/springboot/WebApplication.java
@@ -1,11 +1,12 @@
 package springboot;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.context.web.SpringBootServletInitializer;
 
-@SpringBootApplication
+@EnableAutoConfiguration
 public class WebApplication extends SpringBootServletInitializer {
     @Override
     protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
